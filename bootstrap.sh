3df1d7f68df0b4fd9fcb5c5f7061dfb27ce69496
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-04-09 16:23:00 -0400

    Configure MinKNOW data/log dir and UI

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# Modified from: https://github.com/sirselim/jetson_nanopore_sequencing/blob/main/setup-guide-mk1c.txt
 
 set -o errexit
 set -o nounset
@@ -7,15 +9,18 @@ set -o pipefail
 RED='\033[0;31m'
 NC='\033[0m'
 
-# Modified from: https://github.com/sirselim/jetson_nanopore_sequencing/blob/main/setup-guide-mk1c.txt
+echo -n "Enter the data and log directory: "
+read -r DATA_DIR
+
 RELEASE="$(grep UBUNTU_CODENAME /etc/os-release | cut -d= -f2)"
 if [[ "$RELEASE" != "xenial" ]] && [[ "$RELEASE" != "bionic" ]]; then
     echo -e "${RED}'${RELEASE}' is not supported directly, falling back to 'bionic' packages.${NC}"
     RELEASE="bionic"
 fi
 
+# Install MinKNOW software
 sudo apt update
-sudo apt install -y apt-transport-https gnupg2 vim wget
+sudo apt install -y apt-transport-https gnupg2 wget
 
 wget -O- https://mirror.oxfordnanoportal.com/apt/ont-repo.pub | sudo apt-key add -
 echo "deb [arch=arm64] https://cdn.oxfordnanoportal.com/apt $RELEASE-stable-mk1c non-free" | sudo tee /etc/apt/sources.list.d/nanoporetech.sources.list
@@ -28,3 +33,23 @@ sudo apt install -y \
     ont-configuration-customer-mk1c \
     ont-kingfisher-ui-mk1c \
     ont-vbz-hdf-plugin
+
+# Configure data and log directories
+sudo /opt/ont/minknow/bin/config_editor --conf user \
+  --filename /opt/ont/minknow/conf/user_conf \
+  --set output_dirs.base="$DATA_DIR/data" \
+  --set output_dirs.logs="$DATA_DIR/logs"
+
+# Configure MinKNOW (override MinIONMK1C defaults)
+sudo sed -i -e 's/Serial=.*/Serial=nanopi/g' /etc/oxfordnanopore/configs/identity.config
+sudo sed -i \
+    -e 's/"device_type":.*/"device_type": "MinION",/' \
+    -e 's/"host_type":.*/"host_type": "nanopi",/' \
+    /opt/ont/minknow/conf/app_conf
+
+sudo sed -i \
+    -e 's/--hide-mouse-cursor//g' \
+    -e 's/--keyboard//g' \
+    -e 's/--zoom-factor 2/--zoom-factor 1/g' \
+    /etc/xprofile \
+    /usr/share/applications/minknow.desktop
